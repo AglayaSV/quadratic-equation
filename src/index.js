0a13088a42c10dc95ee184b7dca6c3c4c3363fb7
aglaya <aglayastunkus1@gmail.com>
2018-09-12 14:55:05 +0300

    solution #2 with regExpression

--- src/index.js
+++ src/index.js
@@ -1,71 +1,14 @@
 module.exports = function solveEquation(equation) {
-
-    let aArr=[];
-
-    let eqArr = equation.split('');
-
-    let indexA = equation.search(/x/i);
-    let checkMinus = false;
-
-    for ( let i = 0; i<=indexA; i++){
-
-        aArr.push(eqArr[i]);
-
-    }
-
-    let aCutIndex = aArr.slice(-4);
-    let   aCutArr = aArr.slice(0, aArr.length - aCutIndex.length);
-
-    let A = +aCutArr.join('');
-//B
-    eqArr.splice( 0, aArr.length);
-
-
-    let equation2 = eqArr.join('');
-
-
-    aArr = [];
-    let indexB = equation2.search(/x/i);
-    for ( let i = 0; i<=indexB; i++){
-
-        aArr.push(eqArr[i]);
-
-    }
-
-    if (aArr[3] === '-'){
-        checkMinus = true;
-    }
-    aCutIndex = aArr.slice(-4);
-    aCutArr = aArr.slice(0, aArr.length - aCutIndex.length);
-    aCutArr = aCutArr.slice(5);
-    if (checkMinus === true){
-        aCutArr.unshift('-');
-        checkMinus = false;
-    }
-
-    let B = +aCutArr.join('');
-
-    //C
-    eqArr.splice( 0, aArr.length);
-
-    if (eqArr[1] === '-'){
-        checkMinus = true;
-    }
-    eqArr = eqArr.slice(3);
-    if (checkMinus === true){
-        eqArr.unshift('-');
-        checkMinus = false;
-    }
-
-    let C = +eqArr.join('');
-    let D  =B*B - 4*A*C;
-    let D1 = B*(B-4*A*(C/B));
-
-
-    let x1 = Math.round((-B + Math.sqrt(D))/(2*A));
-    let x2 = Math.round((B*(-1) - Math.sqrt(D))/(2*A));
+    let numbers = equation.match(/\d+/g).map(Number);
+    let operators = equation.match(/[+]|-/g);
+    if (operators.length < 3) operators.unshift('+');
+    let A = +(`${operators[0]}${numbers[0]}`);
+    let B = +(`${operators[1]}${numbers[2]}`);
+    let C = +(`${operators[2]}${numbers[3]}`);
+    let D = B * B - 4 * A * C;
+    let x1 = Math.round((-B + Math.sqrt(D)) / (2 * A));
+    let x2 = Math.round((B * (-1) - Math.sqrt(D)) / (2 * A));
     let result = [x1, x2];
 
-
-    return result.sort(function(a,b){return a - b});
-}
+    return result.sort((a, b) => a - b);
+};
